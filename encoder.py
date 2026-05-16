0e5f2505734cc5798b46237aeddd48b6d5819659
lenar <khabibullin.lsh@phystech.edu>
2026-05-16 22:54:49 +0300

    Change linear global_proj to MLP

--- encoder.py
+++ encoder.py
@@ -63,7 +63,11 @@ class LSTMEncoder(nn.Module):
         intermediate_dim = num_numerical_features + sum(cat_vocab_sizes)
         self.linear = nn.Linear(in_features=intermediate_dim, out_features=hidden_size)
 
-        self.global_proj = nn.Linear(in_features=hidden_size, out_features=embedding_size)
+        self.global_proj = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, embedding_size)
+        )
         self.local_proj = nn.Linear(in_features=hidden_size, out_features=embedding_size)
 
         self.sep_vector = nn.Parameter(torch.empty([self.hidden_size]))
