82b673e472c4bbeed37663d2683e7f1b6aed46c1
lenar <khabibullin.lsh@phystech.edu>
2026-07-13 02:45:05 +0300

    Separate hardcoded hyperparams into yaml

--- run_experiments.py
+++ run_experiments.py
@@ -16,74 +16,53 @@ import dotenv
 dotenv.load_dotenv()
 
 
-hyperparams_distributions = {
-    "hidden_size": [8],
-    "embedding_size": [8],
-    "num_epochs": [1],
-    "margin": [0.5],
-    "learning_rate": [1e-3],
-    "weight_decay": [2e-5],
-    "n_samples_in_batch": [64],
-    "subseq_min": [15],
-    "subseq_max": [150],
-    "k": [5],
-    "cat_features": [["MCC", "trx_category"]],
-    "cat_coverage": [0.9],
-    "classifier": ["logistic_regression"],
-    "optimizer": ["Adam"],
-    "add_sep": [False],
-    "cmlm_lambda": [0.0],
-    "mi_bound_lambda": [1e-5],
-    "club_lr_ratio": [1e-5],
-    "mask_pr": [0.01],
-    "club_pr": [0.01]
-}
+import yaml
+from sklearn.model_selection import ParameterGrid
+
+
+with open("cv_param_grid.yaml") as f:
+    param_grid = list(ParameterGrid(yaml.safe_load(f)))
+
 
 enc_train_dataset, enc_val_dataset, classifier_cv_dataset, test_dataset, vocab_sizes = load_and_split_data(
     "pytorch-lifestream/rosbank-churn", 
     "pytorch-lifestream/rosbank-churn",
-    cat_features=hyperparams_distributions["cat_features"][0],
-    cat_coverage=hyperparams_distributions["cat_coverage"][0],
-    add_sep=hyperparams_distributions["add_sep"][0]
+    cat_features=param_grid[0]["cat_features"][0],
+    cat_coverage=param_grid[0]["cat_coverage"][0],
+    add_sep=param_grid[0]["add_sep"][0]
 )
 
-sampler = ParameterSampler(
-    param_distributions=hyperparams_distributions,
-    n_iter=1
-)
-
+dagshub.init("event-sequence-embeddings", "reizkh")
 device = "cuda" if torch.cuda.is_available() else "cpu"
+rounds = 1
+for hyperparams in param_grid:
+    for _ in range(rounds):
+        with mlflow.start_run() as run:
+            mlflow.log_params(hyperparams)
+            best_encoder = train_encoder(
+                enc_train_dataset,
+                enc_val_dataset,
+                vocab_sizes,
+                hyperparams,
+                run
+            )
 
-# dagshub.init("event-sequence-embeddings", "reizkh")
-# mlflow.config.enable_async_logging()
-# mlflow.set_experiment("Using multiple features")
-for hyperparams in tqdm(sampler, desc="Random search of hyperparameters", leave=False):
-    with mlflow.start_run() as run:
-        mlflow.log_params(hyperparams)
-        best_encoder = train_encoder(
-            enc_train_dataset,
-            enc_val_dataset,
-            vocab_sizes,
-            hyperparams,
-            run
-        )
+            global_eval_ds, global_eval_labels = create_global_dataset(
+                best_encoder,
+                classifier_cv_dataset,
+                device
+            )
+            run_classifier_cv(
+                global_eval_ds,
+                global_eval_labels
+            )
 
-        global_eval_ds, global_eval_labels = create_global_dataset(
-            best_encoder,
-            classifier_cv_dataset,
-            device
-        )
-        run_classifier_cv(
-            global_eval_ds,
-            global_eval_labels
-        )
-
-        local_eval_ds, local_eval_labels = create_local_dataset(
-            best_encoder,
-            classifier_cv_dataset,
-            device,
-        )
-        run_local_cv(
-            local_eval_ds,
-            local_eval_labels
-        )
+            local_eval_ds, local_eval_labels = create_local_dataset(
+                best_encoder,
+                classifier_cv_dataset,
+                device,
+            )
+            run_local_cv(
+                local_eval_ds,
+                local_eval_labels
+            )
